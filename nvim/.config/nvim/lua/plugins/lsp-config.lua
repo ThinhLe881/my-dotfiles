1388ba4e3a3186059b296f00952ddce600bc55a9
ThinhLe881 <thinhleminh201@gmail.com>
2025-01-30 17:39:24 -0500

    add lsp completions

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,18 +9,46 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ts_ls", "clangd", "pylsp" }
+                ensure_installed = { "lua_ls", "ts_ls", "clangd", "pylsp", "jdtls" }
             })
         end
     },
     {
         "neovim/nvim-lspconfig",
+        lazy = false,
         config = function()
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
+            local capabilities = vim.tbl_deep_extend(
+                "force",
+                {},
+                vim.lsp.protocol.make_client_capabilities(),
+                cmp_nvim_lsp.default_capabilities()
+            )
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.ts_ls.setup({})
-            lspconfig.clangd.setup({})
-            lspconfig.pylsp.setup({})
+
+            require('mason-lspconfig').setup_handlers({
+                function(server_name)
+                    -- Don't call setup for JDTLS Java LSP because it will be setup from a separate config
+                    if server_name ~= 'jdtls' then
+                        lspconfig[server_name].setup({
+                            capabilities = capabilities,
+                        })
+                    end
+                end
+            })
+
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.clangd.setup({
+                capabilities = capabilities
+            })
+            lspconfig.pylsp.setup({
+                capabilities = capabilities
+            })
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})

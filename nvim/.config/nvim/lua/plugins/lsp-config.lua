ff8a5e0b8105ec4231f30142e7d0059ae9dc25ae
ThinhLe881 <thinhleminh201@gmail.com>
2025-01-31 15:20:03 -0500

    add more config to telescope

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -14,7 +14,13 @@ return {
         },
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ts_ls", "clangd", "pylsp", "jdtls" }
+                ensure_installed = {
+                    "lua_ls",
+                    "ts_ls",
+                    "clangd",
+                    "pylsp",
+                    "jdtls",
+                }
             })
         end
     },
@@ -72,9 +78,9 @@ return {
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>cr", vim.lsp.buf.references, {})
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-            vim.keymap.set("n", "<leader>fg", vim.lsp.buf.format, {})
+            vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
             vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
         end
     }
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,15 +5,52 @@ return {
     {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.5",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+            },
+        },
         config = function()
-            require("telescope").setup()
+            local file_ignore_patterns = { ".git", "venv", "env", "__pycache__", "node_modules" }
+
+            require("telescope").setup({
+                defaults = {
+                    file_ignore_patterns = file_ignore_patterns
+                },
+                pickers = {
+                    find_files = {
+                        hidden = true,
+                        theme = "ivy"
+                    },
+                    live_grep = {
+                        file_ignore_patterns = file_ignore_patterns,
+                        additional_args = function(_)
+                            return { "--hidden" }
+                        end,
+                        theme = "ivy"
+                    },
+                    buffers = {
+                        theme = "ivy"
+                    }
+                },
+                extensions = {
+                    "fzf"
+                }
+            })
             local builtin = require("telescope.builtin")
 
             vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-            vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fc", function()
+                builtin.find_files {
+                    cwd = vim.fn.stdpath("config")
+                }
+            end)
+            vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
             vim.keymap.set("n", "<leader><leader>", builtin.buffers, {})
 
+            require("telescope").load_extension("fzf")
             require("telescope").load_extension("ui-select")
         end
     }

3b71432ec1690d54d76f0ccaddff9bcd40ee6887
ThinhLe881 <thinhleminh201@gmail.com>
2025-01-31 13:08:28 -0500

    add copilot to nvim

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,12 +1,17 @@
 return {
     {
         "williamboman/mason.nvim",
+        lazy = false,
         config = function()
             require("mason").setup()
         end
     },
     {
         "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
         config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = { "lua_ls", "ts_ls", "clangd", "pylsp", "jdtls" }
